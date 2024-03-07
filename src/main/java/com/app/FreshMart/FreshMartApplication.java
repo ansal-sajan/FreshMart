3aac8102aad06a7b2537e15e099e1170cb92e5fb
Ansal Sajan <ansalsajan1235@gmail.com>
2024-03-07 13:33:18 +0000

    some files added

---
+++ src/main/java/com/app/FreshMart/FreshMartApplication.java
@@ -0,0 +1,13 @@
+package com.app.FreshMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshMartApplication.class, args);
+	}
+
+}
